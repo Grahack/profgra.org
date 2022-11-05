b8d87cf0811844a12327123542bc5fdb19cb6889
Christophe Gragnic <cgragnic@netc.fr>
2022-11-05 11:38:25 +0100

    Link style attempt (but file not loaded)

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -1,22 +1,33 @@
 mport Link from 'next/link';  // doesn't seem to be loaded! (removed the i)
 
-export default function CustomLink({ children, href }) {
-    // If the link is local it will start with a "/"
-    // Otherwise it'll be something like "https://"
-    return href.startsWith('/') || href === '' ? (
-        <Link href={href} className={"tuancao"}>
-            <a>
+xport default function CustomLink({ children, href }) {
+    if href.startsWith('http') {
+      if href.includes('wikipedia') {
+          console.log("wp");
+        return <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={"link-wp"}
+          >{"WP"}
+              {children}
+          </a>
+      }
+          console.log("ext");
+      return <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={"link-ext"}
+          >"ext"
+              {children}
+          </a>
+    }
+          console.log("int");
+    return <Link href={href} className={"tuancao"}>
+            <a>"int"
                 {children}
             </a>
         </Link>
-    ) : (
-        <a
-            href={href}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={"tuan"}
-        >
-            {children}
-        </a>
-    );
+    }
 }
